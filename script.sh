861d8f2a29e9a7aa1cef84820c0f77a8f1763a81
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2019-04-14 12:38:18 +0530

    demo for artefacts archive -2

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@ echo "[INFO] running script available in GitHub via Jenkins"
 echo "[INFO] demo for running script"
 
 echo "[INFO] demo for artefacts archive"
+echo "[INFO] demo for artefacts archive - 2"
